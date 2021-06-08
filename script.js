b791468b15a5530d9a5768d38ea81896b00b13e2
gauravtha2406 <gauravtha2406@gmail.com>
2021-06-09 00:46:17 +0530

    first commit yogabuddy

--- script.js
+++ script.js
@@ -90,3 +90,17 @@
     });
   });
 })(jQuery);
+
+$(document).ready(function(){
+    lightbox.option({
+               'wrapAround': false
+      })
+})
+
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
